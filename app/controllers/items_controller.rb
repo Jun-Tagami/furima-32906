df17335e2d37fe9c93b4ceb5aefefcf2a9bac66a
Jun <zxc4627@gmail.com>
2021-04-25 16:16:55 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,11 @@ class ItemsController < ApplicationController
     end
   end
 
+ def show
+  @item = Item.find(params[:id])
+  @user = User.find(params[:id])
+ end 
+ 
   private
   def items_params
     params.require(:item).permit(:category_id, :status_id, :delivery_fee_id, :area_id, :ship_id, :image, :product_name,
