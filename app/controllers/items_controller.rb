d5209c60638c63a93822c5570ab3e18a404081bd
Jun <zxc4627@gmail.com>
2021-04-27 20:10:10 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -22,10 +22,25 @@ class ItemsController < ApplicationController
   @item = Item.find(params[:id])
  end 
  
+ def edit
+  @item = Item.find(params[:id])
+ end 
+
+ def update
+  # if update
+  # item = Item.find(params[:id])
+  # item.update(items_params)
+  @item = Item.find(params[:id])
+  if @item.update(items_params)
+    redirect_to root_path
+  else
+    render :edit
+  end
+ end
+
   private
   def items_params
-    params.require(:item).permit(:category_id, :status_id, :delivery_fee_id, :area_id, :ship_id, :image, :product_name,
-                                 :explanation, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:category_id, :status_id, :delivery_fee_id, :area_id, :ship_id, :image, :product_name,:explanation, :price).merge(user_id: current_user.id)
   end
 
 end
